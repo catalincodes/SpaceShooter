43f5059f446db5e5e390b997f7f455aab41c41eb
Catalin Popescu <catalinp86@gmail.com>
2016-08-14 21:23:26 -0400

    Added asteroids and attached collider to asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.tag == "Boundaries") {
+			return;
+		} else {
+			//Check who it touched
+			Debug.Log (other.name);
+
+			//Destroys the bolt
+			Destroy (other.gameObject);
+
+			//Destroys the asteroid
+			Destroy (gameObject);
+		}
+	}
+}
